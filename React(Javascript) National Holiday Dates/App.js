d52161f02664582246665f1a2e5ecd13a0f67c3f
EmirAliOner <emiralioner@gmail.com>
2024-08-15 14:45:03 +0300

    React(Javascript)National Holiday Dates
    At the same time, this project was developed with javascript.

---
+++ React(Javascript) National Holiday Dates/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+import data from './data';
+
+function App() {
+  const [holidays, setHolidays] = useState(data);
+  return (
+    <main>
+      <section className='container'>
+        <h3>National Holidays Dates</h3>
+        {holidays.map((holiday) => {
+          return (
+            <div key={holiday.id} className="holiday">
+              <img src={holiday.img} />
+              <div>
+                <p>{holiday.title}</p>
+                <p>{holiday.date}</p>
+              </div>
+            </div>
+          )
+        })}
+        <button onClick={()=> setHolidays([])}>Clear All</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
